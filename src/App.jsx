663f57d39e5d5d4265519c5908ad23c8ceb666dc
aakashwbd <aakash.wbd@gmail.com>
2023-11-03 01:19:59 +0600

    Project Done.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import ImageCard from "./components/ImageCard";
 import { MOCK_DATA } from "./constant/mockData";
 
 const App = () => {
+  /**
+   * assign MOCK_DATA into local state for main data state
+   */
   const [mockData, setMockData] = useState(MOCK_DATA);
 
+  /**
+   * Drag n drop for list sorting
+   */
   const dragItem = useRef(0);
   const draggedOverItem = useRef(0);
 
@@ -17,15 +23,80 @@ const App = () => {
     setMockData(data);
   };
 
+  /**
+   * Single selected data
+   */
+  const [selectedData, setSelectedData] = useState([]);
+
+  const selectHandler = (item, checked) => {
+    setSelectedData((prevState) => {
+      if (checked) {
+        return [...prevState, item];
+      } else {
+        return prevState.filter((fItem) => fItem !== item);
+      }
+    });
+  };
+
+  /**
+   * Multiple select handler for select all item
+   */
+  const parentSelectHandler = (checked) => {
+    let arr = [...mockData];
+    if (checked) {
+      setSelectedData(arr);
+    } else {
+      setSelectedData([]);
+    }
+  };
+
+  /**
+   * Select All check handler
+   */
+  const parentSelectorCheckedHandler = (value) => {
+    let fields = [];
+    value.map((item) => fields.push({ field: item, found: false }));
+
+    fields.forEach((fItem) => {
+      let find = selectedData.find((item) => item === fItem.field);
+      if (find) {
+        fItem["found"] = true;
+      }
+    });
+    return !fields.some((item) => item.found === false);
+  };
+
+  /**
+   * Remove or Delete selected data from mockData state by deleteHandler
+   */
+  const deleteHandler = () => {
+    let data = [...mockData];
+    if (selectedData?.length) {
+      data = data.filter((el) => !selectedData.includes(el));
+    }
+    setMockData(data);
+    setSelectedData([]);
+  };
+
   return (
     <div className="p-2 lg:p-4 2xl:p-10">
       <div className="w-full md:w=[90%] 2xl:w-[80%] mx-auto shadow-xl rounded-md border">
         <div className="flex items-center justify-between px-6 py-3 border-b">
           <label className="inline-flex items-center gap-2">
-            <input type="checkbox" />
-            <span>3 File Selected</span>
+            <input
+              type="checkbox"
+              onChange={(e) => parentSelectHandler(e.target.checked)}
+              checked={parentSelectorCheckedHandler(mockData)}
+            />
+            <span>
+              {!selectedData?.length
+                ? "Select All"
+                : `${selectedData?.length} file selected.`}
+            </span>
           </label>
-          <button className="text-red-500">Delete files</button>
+          <button className="text-red-500" onClick={deleteHandler}>
+            Delete files
+          </button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-5 items-start px-6 py-3 gap-4">
           {mockData.map((item, i) => (
@@ -38,7 +109,12 @@ const App = () => {
               onDragEnd={sortHandler}
               onDragOver={(e) => e.preventDefault()}
             >
-              <ImageCard src={item?.attachemnt} alt={item?.name} />
+              <ImageCard
+                src={item?.attachemnt}
+                alt={item?.name}
+                onChange={(checked) => selectHandler(item, checked)}
+                checked={selectedData.some((mItem) => mItem === item)}
+              />
             </div>
           ))}
         </div>
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
-const ImageCard = ({ src = "", alt = "" }) => {
+const ImageCard = ({
+  src = "",
+  alt = "",
+  checked = false,
+  onChange = (checked) => {},
+}) => {
   return (
     <div
       className={`cursor-move border rounded-md overflow-hidden relative group w-full h-full`}
     >
-      <div class="absolute inset-0 bg-[rgba(0,0,0,0.5)] opacity-0 group-hover:opacity-100 transition-opacity"></div>
-      <input type="checkbox" className="absolute top-3 left-3" />
+      <div
+        className={`absolute inset-0 ${
+          !checked
+            ? "bg-[rgba(0,0,0,0.5)] opacity-0 group-hover:opacity-100 transition-opacity"
+            : "bg-[rgba(0,0,0,0.1)]"
+        }`}
+      ></div>
+      <input
+        type="checkbox"
+        className="absolute top-3 left-3"
+        onChange={(e) => onChange(e.target.checked)}
+        checked={checked}
+      />
       <img src={src} alt={alt} className="w-full h-full" />
     </div>
   );
