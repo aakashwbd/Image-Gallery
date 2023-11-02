beca87cb921cbb409bfbc84999b51dd502fda1f8
aakashwbd <aakash.wbd@gmail.com>
2023-11-02 17:17:28 +0600

    drag enent added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ImageCard from "./components/ImageCard";
 import { MOCK_DATA } from "./constant/mockData";
 
 const App = () => {
+  const [mockData, setMockData] = useState(MOCK_DATA);
+
+  const dragItem = useRef(0);
+  const draggedOverItem = useRef(0);
+
+  const sortHandler = () => {
+    const data = [...mockData];
+    const temp = data[dragItem.current];
+    data[dragItem.current] = data[draggedOverItem.current];
+    data[draggedOverItem.current] = temp;
+
+    setMockData(data);
+  };
+
   return (
     <div className="p-2 lg:p-4 2xl:p-10">
       <div className="w-full md:w=[90%] 2xl:w-[80%] mx-auto shadow-xl rounded-md border">
@@ -14,10 +28,15 @@ const App = () => {
           <button className="text-red-500">Delete files</button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-5 items-start px-6 py-3 gap-4">
-          {MOCK_DATA.map((item, i) => (
+          {mockData.map((item, i) => (
             <div
               key={i}
               className={i === 0 ? "md:row-span-2 md:col-span-2" : ""}
+              draggable
+              onDragStart={() => (dragItem.current = i)}
+              onDragEnter={() => (draggedOverItem.current = i)}
+              onDragEnd={sortHandler}
+              onDragOver={(e) => e.preventDefault()}
             >
               <ImageCard src={item?.attachemnt} alt={item?.name} />
             </div>
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const ImageCard = ({ src = "", alt = "" }) => {
   return (
     <div
-      draggable
       className={`cursor-move border rounded-md overflow-hidden relative group w-full h-full`}
     >
       <div class="absolute inset-0 bg-[rgba(0,0,0,0.5)] opacity-0 group-hover:opacity-100 transition-opacity"></div>
